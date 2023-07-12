18faea60a804a9a816e7b198e5848bd8a7870cc6
Colin Leroy-Mira <colin.leroy-mira@sigfox.com>
2023-07-12 07:28:06 +0200

    Fix set_termios prototype for kernel 6

--- module/tty0tty.c
+++ module/tty0tty.c
@@ -288,7 +288,7 @@ exit:
 #define RELEVANT_IFLAG(iflag) ((iflag) & (IGNBRK|BRKINT|IGNPAR|PARMRK|INPCK))
 
 static void tty0tty_set_termios(struct tty_struct *tty,
-				struct ktermios *old_termios)
+				const struct ktermios *old_termios)
 {
 	unsigned int cflag;
 	unsigned int iflag;
